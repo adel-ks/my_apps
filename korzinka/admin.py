d6d75ab5f7d77c62f196589027bf71de12aede2d
adel-ks <adelmanaeva@gmail.com>
2021-04-26 13:06:48 +0200

    Added in the file 'admin' visual editor

--- korzinka/admin.py
+++ korzinka/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Product
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
-admin.site.register(Product)
+
+class ProductAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('id','name','category')
+	list_display_links = ('id','name')
+	search_fields = ('name',)
+	list_filter = ('name','category')
+	form = ProductAdminForm
+
+
+admin.site.register(Product,ProductAdmin)
